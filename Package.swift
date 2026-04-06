d0d7691c892b8e7ee8ae1abd679aa749e86e20e1
zzhdong <zzhdong@gmail.com>
2026-04-06 21:41:09 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "TreeSitterLanguages",
+    name: "ETreeSitterLanguages",
     platforms: [.iOS(.v14)],
     products: [
         .library(name: "TreeSitterLanguagesCommon", targets: ["TreeSitterLanguagesCommon"]),
